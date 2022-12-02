e376d0f1263170bda5f4bbba7423e4137b1e9145
Piotr Wachulec <p.wachulec@gmail.com>
2022-12-02 16:58:48 +0100

    Added day 01

---
+++ Day01/Program.cs
@@ -0,0 +1,21 @@
+﻿var acc = 0;
+var callories = new List<int>();
+
+await foreach (var line in File.ReadLinesAsync("./data.txt"))
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        callories.Add(acc);
+        acc = 0;
+    }
+    else
+    {
+        acc += Int32.Parse(line);
+    }
+}
+
+var maxCallories = callories.Max();
+var totalCalloriesOfThreeElves = callories.OrderDescending().Take(3).Sum();
+
+Console.WriteLine($"Max calories: {maxCallories}");
+Console.WriteLine($"Total callories of 3 elves: {totalCalloriesOfThreeElves}");
